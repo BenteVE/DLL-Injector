6f3217feeac00369c6efca64ad3dc5aa5a559503
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-01-29 23:46:23 +0100

    Delete Error.cpp

--- Injector/Error.cpp
+++
@@ -1,7 +0,0 @@
-// display error messages
-#include "Error.h"
-void DisplayError(const char* errorTitle, const char* errorMessage)
-{
-    MessageBox(NULL, errorMessage, errorTitle, NULL);
-    exit(-1);
-}
