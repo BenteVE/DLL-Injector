49b20c5fc35e83956d054726f173c297c91dbb40
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-01-31 15:52:58 +0100

    Handling errors and closing the terminal when unloading the DLL

--- DLL CreateThread/dllmain.cpp
+++ DLL CreateThread/dllmain.cpp
@@ -1,35 +1,53 @@
 #include <Windows.h>
 #include <iostream>
 
-DWORD WINAPI testThread(PVOID base)
-{
+// Pointer to the opened stream
+FILE* stream;
 
-    printf("Hello from inside thread.\n");
-
-    FreeLibraryAndExitThread(static_cast<HMODULE>(base), 1);
+DWORD WINAPI testThread(PVOID base) {
+	fprintf(stream, "DLL loaded.\n");
+	return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        FILE* stream; // An out parameter that will point to the reopened stream when the function returns.
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		// Open console for debugging
+		if (AllocConsole())
+		{
+			SetConsoleTitle(TEXT("Console"));
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 
-        // Open console for debugging
-        if (AllocConsole())
-        {
-            freopen_s(&stream, "CONOUT$", "w", stdout);
-            SetConsoleTitle(L"Console");
-            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-            printf("DLL loaded.\n");
-        }
-        CreateThread(nullptr, NULL, testThread, hModule, NULL, nullptr);
-        break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+			errno_t err;
+			// Reopen stream "stdout" and assign it to the console:
+			err = freopen_s(&stream, "CONOUT$", "w", stdout);
+
+			if (err != 0 || stream == NULL)
+			{
+				FreeConsole();
+				return FALSE;
+			}
+
+			CreateThread(nullptr, NULL, testThread, hModule, NULL, nullptr);
+		}
+		else {
+			// Indicate DLL loading failed
+			return FALSE;
+		}
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		if (stream != NULL) {
+			fprintf(stream, "DLL Process Detach\n");
+			fclose(stream);
+		}
+		FreeConsole();
+		break;
+	}
+	return TRUE;
 }
