91ad271581c5becb79c5aa37cb41d09ffc4d8993
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-02-02 17:03:40 +0100

    Using NOMINMAX

--- Injector/parser.h
+++ Injector/parser.h
@@ -1,16 +1,9 @@
 #pragma once
+#define NOMINMAX // excludes min and max macros in windef.h
 #include <Windows.h>
+
 #include "logger.h"
 #include "GetProcId.h"
 #include <string>
-
-// undefine macros that cause errors in argparser
-#ifdef max
-#undef max
-#endif
-
-#ifdef min
-#undef min
-#endif
 
 #include "cxxopts.hpp"
