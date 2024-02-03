7006b1e6ea7b551246c01320101030969a153f4d
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-02-03 12:08:41 +0100

    Separated OpenConsole function

--- DLL CreateThread/dllmain.cpp
+++ DLL CreateThread/dllmain.cpp
@@ -9,44 +9,52 @@ DWORD WINAPI testThread(PVOID base) {
 	return 0;
 }
 
+// Open console for debugging
+BOOL openConsole() {
+	
+	if (!AllocConsole())
+	{
+		return FALSE;
+	}
+	SetConsoleTitle(TEXT("Console"));
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+	errno_t err;
+	// Reopen stream "stdout" and assign it to the console:
+	err = freopen_s(&stream, "CONOUT$", "w", stdout);
+
+	if (err != 0 || stream == NULL)
+	{
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+void clean() {
+	if (stream != NULL) {
+		fclose(stream);
+	}
+	FreeConsole();
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		// Open console for debugging
-		if (AllocConsole())
-		{
-			SetConsoleTitle(TEXT("Console"));
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-
-			errno_t err;
-			// Reopen stream "stdout" and assign it to the console:
-			err = freopen_s(&stream, "CONOUT$", "w", stdout);
-
-			if (err != 0 || stream == NULL)
-			{
-				FreeConsole();
-				return FALSE;
-			}
-
-			CreateThread(nullptr, NULL, testThread, hModule, NULL, nullptr);
-		}
-		else {
+		if (!openConsole()) {
 			// Indicate DLL loading failed
 			return FALSE;
 		}
+		CreateThread(nullptr, NULL, testThread, hModule, NULL, nullptr);
 		break;
 	case DLL_THREAD_ATTACH:
 		break;
 	case DLL_THREAD_DETACH:
 		break;
 	case DLL_PROCESS_DETACH:
-		if (stream != NULL) {
-			fprintf(stream, "DLL Process Detach\n");
-			fclose(stream);
-		}
-		FreeConsole();
+		clean();
 		break;
 	}
 	return TRUE;
